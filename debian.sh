8688c713f725276d2ac44f3f68d91dd355cb746a
Elieroc <34548349+Elieroc@users.noreply.github.com>
2025-04-28 16:58:51 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -6,7 +6,7 @@
 #########################
 
 function conf_few_tools {
-        apt update && apt install -y curl gpg
+        apt update && apt install -y ca-certificates curl gpg
         wget https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-amd64.deb && apt install -y ./fastfetch-linux-amd64.deb && rm ./fastfetch-linux-amd64.deb
 }
 
@@ -27,7 +27,17 @@ function conf_motd {
         curl https://..../custom-motd -o /etc/motd/99-custom
 }
 
-#conf_few_tools
-#conf_sudo
-#conf_fish
-#conf_motd
+function conf_docker {
+        apt install -y ca-certificates && install -m 0755 -d /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg && chmod a+r /etc/apt/keyrings/docker.gpg && echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null && apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+}
+
+function main {
+
+        read -p "Install Docker ? (y/n): " res
+        if [[ "$res" == "y" || "$res" == "Y" ]]; then conf_docker; fi;
+
+        #conf_few_tools
+        #conf_sudo
+        #conf_fish
+        #conf_motd
+}
